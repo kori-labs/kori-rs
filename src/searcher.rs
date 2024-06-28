42efbce60906ab8bd314cef6c8e6a3c874bf1258
A <a@kori.network>
2024-06-28 17:54:53 +0200

    update grpc protos

--- src/searcher.rs
+++ src/searcher.rs
@@ -5,7 +5,7 @@ use crate::proto;
 
 use crate::proto::auth::Token;
 use crate::proto::searcher::searcher_service_client::SearcherServiceClient;
-use crate::proto::searcher::{AddressSubscriptionV0, MempoolSubscription, PackageSubscriptionV0, SendBundleResponse, SharedObjectSubscriptionV0};
+use crate::proto::searcher::{AddressSubscriptionV0, EventSubscriptionV0, GetTipAddressesRequest, GetTipAddressesResponse, MempoolSubscription, PackageSubscriptionV0, SendBundleResponse, SharedObjectSubscriptionV0};
 
 pub struct KoriSearcher {
     searcher_client: SearcherServiceClient<Channel>,
@@ -31,6 +31,7 @@ impl KoriSearcher {
         packages: Vec<String>,
         objects: Vec<String>,
         addresses: Vec<String>,
+        events: Vec<String>,
         on_data: F,
     ) -> Result<(), Box<dyn std::error::Error>>
         where
@@ -40,6 +41,7 @@ impl KoriSearcher {
             packages: if !packages.is_empty() { Some(PackageSubscriptionV0 { package: packages }) } else { None },
             objects: if !objects.is_empty() { Some(SharedObjectSubscriptionV0 { object: objects }) } else { None },
             addresses: if !addresses.is_empty() { Some(AddressSubscriptionV0 { address: addresses }) } else { None },
+            events: if !events.is_empty() { Some(EventSubscriptionV0 { event: events }) } else { None },
         });
 
         if let Some(access_token) = &self.access_token {
@@ -75,6 +77,23 @@ impl KoriSearcher {
         }
 
         let response = self.searcher_client.send_bundle(request).await?;
+
+        Ok(response.into_inner())
+    }
+
+    pub async fn get_tip_addresses(&mut self) -> Result<GetTipAddressesResponse, Box<dyn std::error::Error>> {
+        let mut request = tonic::Request::new(GetTipAddressesRequest::default());
+
+        if let Some(access_token) = &self.access_token {
+            request.metadata_mut().insert(
+                "authorization",
+                tonic::metadata::MetadataValue::from_str(
+                    &format!("Bearer {}", access_token.value)
+                )?,
+            );
+        }
+
+        let response = self.searcher_client.get_tip_addresses(request).await?;
 
         Ok(response.into_inner())
     }
